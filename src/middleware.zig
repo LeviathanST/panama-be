5a60b57169cacc694c3728d9efb063b6708fd77e
LeviathanST <phamquochung170505@gmail.com>
2025-06-05 21:47:48 +0700

    update(mw/auth): verify token before handle route

--- src/middleware.zig
+++ src/middleware.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const tk = @import("tokamak");
 const pg = @import("pg");
+const util = @import("util.zig");
+const Config = @import("Config.zig");
 const GeneralError = @import("App.zig").GeneralError;
 const User = @import("model").User;
 
@@ -9,20 +11,28 @@ pub const Auth = struct {
     pub const Error = error{Unauthorized} || User.FindError;
 
     pub const Data = struct {
-        account_id: u64,
+        account_id: i32,
     };
 
     pub fn @"fn"(ctx: *tk.Context) anyerror!Data {
-        const pool = try ctx.injector.get(*pg.Pool);
-        // TODO: change to Bearer token
-        const account_id = ctx.req.header("authorization") orelse return Error.Unauthorized;
-        if (!(try User.existedById(pool, 1))) return Error.UserNotFound;
+        const p = try ctx.injector.get(*pg.Pool);
+        const map = try ctx.injector.get(*std.StringHashMap([]const u8));
+        const config = try ctx.injector.get(*Config);
+
+        const header = ctx.req.header("authorization") orelse return Error.Unauthorized;
+        var splits = std.mem.splitScalar(u8, header, ' ');
+        if (!std.mem.eql(u8, splits.first(), "Bearer")) return Error.Unauthorized;
+
+        const claims = try util.token.verify(
+            ctx.req.arena,
+            map.*,
+            config.app.at_secret,
+            splits.next() orelse return Error.Unauthorized,
+            .a,
+        );
+        const account_id = try User.findIdByUsername(p, claims.username);
         return .{
-            .account_id = try std.fmt.parseInt(
-                u64,
-                account_id,
-                10,
-            ),
+            .account_id = account_id,
         };
     }
 };
