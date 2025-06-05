c2a9bdc9afba5415d80afc1d94274a2761b8c7f1
LeviathanST <phamquochung170505@gmail.com>
2025-06-05 21:48:57 +0700

    add(model/user): find, insert

--- src/model/User.zig
+++ src/model/User.zig
@@ -2,23 +2,60 @@ const std = @import("std");
 const Pool = @import("pg").Pool;
 
 pub const FindError = error{UserNotFound};
+pub const InsertError = error{UserExisted};
 
-id: u64,
+pub const Self = @This();
+
+id: i32,
 username: []const u8,
 password: []const u8,
 
-pub fn existedById(pool: *Pool, id: u64) !bool {
+pub fn insert(pool: *Pool, username: []const u8, password: []const u8) !void {
     const conn = try pool.acquire();
     defer conn.deinit();
 
-    const row = conn.row(
-        \\ SELECT EXISTS (SELECT 1 FROM "user" WHERE id = $1) as "exists"
-    , .{id}) catch |err| {
+    _ = conn.exec(
+        \\ INSERT INTO "user" (username, password)
+        \\ VALUES ($1, $2)
+    , .{ username, password }) catch |err| {
+        if (conn.err) |pg_err| {
+            std.log.err("PG error message: {s}", .{pg_err.message});
+            if (pg_err.isUnique()) return InsertError.UserExisted;
+        }
+        return err;
+    };
+}
+
+pub fn findIdByUsername(pool: *Pool, username: []const u8) !i32 {
+    const conn = try pool.acquire();
+    defer conn.deinit();
+
+    var row = conn.row(
+        \\ SELECT id FROM "user" WHERE username = $1
+    , .{username}) catch |err| {
         if (conn.err) |pg_err| {
             std.log.err("PG error message: {s}", .{pg_err.message});
         }
         return err;
-    };
+    } orelse return FindError.UserNotFound;
+    defer row.deinit() catch unreachable;
+    return row.getCol(i32, "id");
+}
 
-    return row.?.getCol(bool, "exists");
+pub fn findByUsername(alloc: std.mem.Allocator, pool: *Pool, username: []const u8) !Self {
+    const conn = try pool.acquire();
+    defer conn.deinit();
+
+    var row = conn.row(
+        \\ SELECT * FROM "user" WHERE username = $1
+    , .{username}) catch |err| {
+        if (conn.err) |pg_err| {
+            std.log.err("PG error message: {s}", .{pg_err.message});
+        }
+        return err;
+    } orelse return FindError.UserNotFound;
+    defer row.deinit() catch unreachable;
+
+    const user = try row.to(Self, .{ .map = .name, .allocator = alloc });
+    return user;
 }
