2a7d58c696d66912288a9fe854b561a25a7fafbe
LeviathanST <phamquochung170505@gmail.com>
2025-06-16 00:47:46 +0700

    update field name in create project request

--- src/api/project/create.zig
+++ src/api/project/create.zig
@@ -1,6 +1,9 @@
+const std = @import("std");
 const tk = @import("tokamak");
 const pg = @import("pg");
 const model = @import("model");
+const util = @import("../../util.zig");
+const Config = @import("../../Config.zig");
 const Success = @import("response").Success;
 const Project = @import("model").Project;
 
@@ -10,19 +13,19 @@ const InsertDTO = struct {
     description: []const u8,
     category: []const u8,
     time: ?[]const u8 = null,
-    images: []model.Image.BaseType,
+    image_urls: []model.Image.BaseType,
     video: ?model.Video.BaseType,
 };
 
 pub fn create(p: *pg.Pool, data: InsertDTO) !Success(?u8) {
-    if (data.video == null and data.images.len == 0) return Error.AtLeastOne;
+    if (data.video == null and data.image_urls.len == 0) return Error.AtLeastOne;
     try Project.insert(
         p,
         data.title,
         data.description,
         data.category,
         data.time,
-        data.images,
+        data.image_urls,
         data.video,
     );
     return .{
