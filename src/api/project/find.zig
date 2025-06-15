ce6a5fe3ef34caae0ea149d71a57fe017dfff5dd
LeviathanST <phamquochung170505@gmail.com>
2025-06-16 00:47:27 +0700

    using base_type response in find project

--- src/api/project/find.zig
+++ src/api/project/find.zig
@@ -3,31 +3,19 @@ const pg = @import("pg");
 const tk = @import("tokamak");
 const api = @import("../../api.zig");
 const model = @import("model");
+const base_type = @import("base_type");
 
 const Project = model.Project;
 const Image = model.Image;
 const Video = model.Video;
 
 const Success = @import("response").Success;
-const VideoResponse = struct {
-    url: []const u8,
-    thumbnail: []const u8,
-};
-const ProjectResponse = struct {
-    id: i32,
-    title: []const u8,
-    description: []const u8,
-    category: []const u8,
-    time: ?[]const u8,
-    images_url: [][]const u8,
-    video: ?VideoResponse,
-};
 
-pub fn find(alloc: std.mem.Allocator, pool: *pg.Pool, id: i32) !Success(ProjectResponse) {
+pub fn find(alloc: std.mem.Allocator, pool: *pg.Pool, id: i32) !Success(base_type.ProjectResponse) {
     const project = try Project.find(alloc, pool, id);
     var images = try Image.findManyByProjectId(alloc, pool, id);
     const raw_video = try Video.findByProjectId(alloc, pool, id);
-    const video: ?VideoResponse = blk: {
+    const video: ?base_type.VideoResponse = blk: {
         if (raw_video) |v| {
             break :blk .{ .url = v.url, .thumbnail = v.thumbnail };
         } else break :blk null;
@@ -41,7 +29,7 @@ pub fn find(alloc: std.mem.Allocator, pool: *pg.Pool, id: i32) !Success(ProjectR
             .description = project.description,
             .category = project.category,
             .time = project.time,
-            .images_url = try images.toOwnedSlice(),
+            .images = try images.toOwnedSlice(),
             .video = video,
         },
     };
